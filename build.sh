2325ed0e78626edbaa5b2b1121102f425a380674
panyingyun <panyingyun@gmail.com>
2017-02-18 16:53:50 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 echo "build linux amd64"
-GOOS=linux GOARCH=amd64 go build -o watch_amd64
-tar czvf linux_amd64.tar.gz  watch_amd64
+GOOS=linux GOARCH=amd64 go build
+tar czvf linux_amd64.tar.gz  watch
 
 echo "build linux i386"
-GOOS=linux GOARCH=386 go build -o watch_i386
-tar czvf linux_i386.tar.gz  watch_i386
+GOOS=linux GOARCH=386 go build 
+tar czvf linux_i386.tar.gz  watch
 
 echo "build windows amd64"
-GOOS=windows GOARCH=amd64 go build -o watch_amd64.exe
-tar czvf windows_amd64.tar.gz  watch_amd64.exe
+GOOS=windows GOARCH=amd64 go build -o watch.exe
+tar czvf windows_amd64.tar.gz  watch.exe
 
 echo "build windows i386"
-GOOS=windows GOARCH=386 go build -o watch_i386.exe
-tar czvf windows_i386.tar.gz  watch_i386.exe
+GOOS=windows GOARCH=386 go build -o watch.exe
+tar czvf windows_i386.tar.gz  watch.exe
 
 echo "build Mac OS X 64bit"
 GOOS=darwin GOARCH=amd64 go build
+tar czvf darwin_amd64.tar.gz  watch
